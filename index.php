8b1b1e143e4a9a4f9fce2f5ee40ea67266c15c92
labuca1617daw2 <labuca1617daw2@gmail.com>
2017-03-13 20:36:40 +0100

    "cookies3 commit 1"

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ and open the template in the editor.
 <html> 
     <head> 
     <meta charset="UTF­8"> 
-        <title>Desplegant una aplicació PHP sobre Heroku</title> 
-    </head> 
+            <title>Desplegant una aplicació PHP sobre Heroku</title> 
+        </head> 
     <body> 
-        <?php 
-            echo "Afegint Cookies<br>"; 
-            echo "Sóc el desenvolupador labuca1617daw2"; 
-            setcookie("nom", "Jaume Pons",time()+3600, "/", "", 0); 
-            setcookie("edat", "20",time()+3600, "/", "", 0); 
-            setcookie("cicle", "DAW",time()+3600, "/", "", 0); 
-            setcookie("curs", "2n",time()+3600, "/", "", 0); 
-            setcookie("any", "2016­2017",time()+3600, "/", "", 0);            
-        ?>  
+        <?php
+            echo "Afegint Cookies<br>";
+            echo "Sóc el desenvolupador labuca1617daw2";
+            setcookie("nom","Jaume Pons",time()+3600,"/","", 0);
+            setcookie("edat","20",time()+3600,"/","",0);
+            setcookie("cicle","DAW",time()+3600,"/","",0);
+            setcookie("curs","2n",time()+3600,"/","",0);
+            setcookie("any","2016­2017",time()+3600,"/","",0);
+            ?>  
     </body>    
 </html> 
